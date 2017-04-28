ebf66c4a204a1bc3a1ee945505da32b8f7246c2d
Nguyễn Anh Kiệt <kietnguyen0814@gmail.com>
2017-04-29 00:15:01 +0700

    Flickr Search Final

---
+++ FlickrSearch_Kiet_13110085/FlickrSearchResults.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrSearchResults.swift
+//  FlickrSearch_Kiet_13110085
+//
+//  Created by Kiet Nguyen on 4/29/17.
+//  Copyright © 2017 Kiet Nguyen. All rights reserved.
+//
+
+import Foundation
+
+struct FlickrSearchResults {
+    let searchTerm : String
+    let searchResults : [FlickrPhoto]
+}
